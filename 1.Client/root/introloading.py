91db182a2f3e4e0f12989ec74d166c0d4ac9115f
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2019-05-24 19:30:48 +0300

    Add files via upload

---
+++ 1.Client/root/introloading.py
@@ -0,0 +1,19 @@
+#Find
+			self.loadingGage.SetPercentage(2+98*p/100, 100)
+			
+#Change
+			self.loadingGage.SetPercentage(p, 160)
+		
+#Find
+	def LoadData(self, playerX, playerY):
+	
+#Add Above
+	def bos(self):
+		pass
+		
+#Find
+		self.__SetProgress(0)
+		
+#Add Above		
+		checkin, _ = zip(*self.loadStepList)
+		self.loadStepList = sorted(self.loadStepList+[(i, ui.__mem_func__(self.bos)) for i in range(min(checkin), max(checkin)+1) if i not in checkin])
